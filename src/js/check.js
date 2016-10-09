8e54cf079fd7c7e412bef0cbb450d0279b9bc680
Grigory Manusha <grigory.manusha@gmail.com>
2016-10-09 16:12:16 +0300

    Правка функции getMessage

--- src/js/check.js
+++ src/js/check.js
@@ -1,6 +1,13 @@
 "use strict";
 
 var getMessage = function(a, b){
+
+    var distancePath = 0;
+    var multiplications;
+    var numberOfSteps = 0;
+    var i;
+
+    // Булевный
     if (typeof a === "boolean") {
         if (a === true) {
             return "Я попал в " + b;
@@ -8,30 +15,31 @@ var getMessage = function(a, b){
             return "Я никуда не попал";
         }
 
+    // Число
     } else if (typeof a === "number"){
         return "Я прыгнул на " + a * 100 + " сантиметров";
-    } else if (Array.isArray(a) && Array.isArray(b)){
 
-        var distancePath = 0;
-        var multiplications = a.map(function(elem, index){
+    // Оба массивы
+    } else if (Array.isArray(a) && Array.isArray(b)){
+        multiplications = a.map(function(elem, index){
             if (typeof elem === "number" && typeof b[index] === "number") {
                 return elem * b[index];
             }
         });
-
-        for (var i = 0; i < multiplications.length; i++) {
+        for (i = 0; i < multiplications.length; i++) {
             distancePath += multiplications[i];
         }
-
         return "Я прошёл " + distancePath + " метров";
 
+    // Единственный первый массив
     } else if (Array.isArray(a)){
-        var numberOfSteps = 0;
-        for (var i = 0; i < a.length; i++) {
+        for (i = 0; i < a.length; i++) {
            numberOfSteps += a[i];
         }
         return "Я прошёл " + numberOfSteps + " шагов";
-    }else {
+
+    // Ошибка
+    } else {
         return "Переданы некорректные данные";
     }
 };
