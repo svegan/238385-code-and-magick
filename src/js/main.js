5bbe83555145d3838285e89d126bdbdbd268afe6
Grigory Manusha <grigory.manusha@gmail.com>
2016-11-12 12:38:37 +0300

    Запомнить всё (Дополнительное задание)

--- src/js/main.js
+++ src/js/main.js
@@ -10,13 +10,19 @@ define(['./modules/game', './modules/init', './modules/reviews', './modules/gall
 
   var galleryImages = document.querySelectorAll('.photogallery-image');
   var images = Array.prototype.map.call(galleryImages, function(elem) {
-    return elem.firstChild.src;
+    return elem.firstChild.attributes.src.nodeValue;
   });
   var galleryContainer = document.querySelector('.overlay-gallery');
   var gallery = new Gallery(galleryContainer, images);
-  Array.prototype.forEach.call(galleryImages, function(elem, index) {
+  Array.prototype.forEach.call(galleryImages, function(elem) {
     elem.addEventListener('click', function() {
-      gallery.show(index);
+      location.hash = '#photo/' + elem.firstChild.attributes.src.nodeValue;
     });
+  });
+  window.addEventListener('load', function() {
+    if (!location.hash) {
+      return;
+    }
+    gallery.onHashChange();
   });
 });
--- src/js/modules/gallery.js
+++ src/js/modules/gallery.js
@@ -13,25 +13,29 @@ define(['./inherit', './baseDOM'], function(inherit, baseDOM) {
     this._onCloseClick = this._onCloseClick.bind(this);
     this._onBackwardClick = this._onBackwardClick.bind(this);
     this._onForwardClick = this._onForwardClick.bind(this);
+    this.onHashChange = this.onHashChange.bind(this);
+    window.addEventListener('hashchange', this.onHashChange);
   };
 
   inherit(Gallery, baseDOM);
 
-  Gallery.prototype.show = function(number) {
+  Gallery.prototype.show = function(param) {
     this.close.addEventListener('click', this._onCloseClick);
     this.backward.addEventListener('click', this._onBackwardClick);
     this.forward.addEventListener('click', this._onForwardClick);
     this.element.classList.remove('invisible');
     this.total.textContent = this.pictures.length;
-    this.setActivePicture(number);
+    this.setActivePicture(param);
   };
   Gallery.prototype.hide = function() {
     this.element.classList.add('invisible');
     this.close.removeEventListener('click', this._onCloseClick);
     this.backward.removeEventListener('click', this._onBackwardClick);
     this.forward.removeEventListener('click', this._onForwardClick);
+    location.hash = '';
   };
-  Gallery.prototype.setActivePicture = function(number) {
+  Gallery.prototype.setActivePicture = function(param) {
+    var number = typeof param === 'string' ? this.pictures.indexOf(param) : param;
     this.activePicture = number;
     var preview = document.querySelector('.overlay-gallery-preview');
     var existImage = preview.querySelector('img');
@@ -49,13 +53,27 @@ define(['./inherit', './baseDOM'], function(inherit, baseDOM) {
   };
   Gallery.prototype._onBackwardClick = function() {
     if (this.activePicture >= 1) {
-      this.setActivePicture(this.activePicture - 1);
+      location.hash = '#photo/' + this.pictures[this.activePicture - 1];
     }
   };
   Gallery.prototype._onForwardClick = function() {
     if (this.activePicture < this.pictures.length - 1) {
-      this.setActivePicture(this.activePicture + 1);
+      location.hash = '#photo/' + this.pictures[this.activePicture + 1];
     }
+  };
+  Gallery.prototype.onHashChange = function() {
+    var regExp = /#photo\/(\S+)/;
+    var address = location.hash.match(regExp);
+    address = address ? address[0].replace('#photo/', '') : null;
+    if (address) {
+      this.show(address);
+    } else {
+      this.hide();
+    }
+  };
+  Gallery.prototype.remove = function() {
+    this.hide();
+    window.removeEventListener('hashchange', this.onHashChange);
   };
   return Gallery;
 });
